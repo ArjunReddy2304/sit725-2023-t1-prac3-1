211f126b9cecad86f44f45833372a31f4ad16af8
ArjunReddy2304 <reddy.malli13@gmail.com>
2023-08-01 22:29:11 +1000

    Week 3

---
+++ server.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let app= express();
+let port=process.env.port || 3000;
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/',(req,res)=>{
+    res.render('index.html');
+});
+
+
+  app.listen(port, ()=>{
+    console.log('Server Started');
+});
